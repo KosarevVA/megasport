2fa516478d6728b8fd1d51baec85d1f16ed67e68
Vlad Kosarev <kva3292011@yandex.ru>
2022-09-26 21:06:58 +0300

    Добавил роуты для регистрации

--- app/routes.php
+++ app/routes.php
@@ -3,5 +3,7 @@
 use App\Modules\System\Route;
 
 return [
-	'example/{int}/{string}' => new Route('Example', 'example'),
+	'/megasport/example/{int}/{string}' => new Route('Example', 'example'),
+	'/megasport/signup/' => new Route('Signup', 'index'),
+	'/megasport/signup/do' => new Route('Signup', 'signUp'),
 ];
