ce04a89d44f541ef3a3e7bbe2e3c12156a8c6baa
Vlad Kosarev <kva3292011@yandex.ru>
2022-09-27 19:59:21 +0300

    Добавил плэйсходер тайтла в шапке сайта

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -4,7 +4,7 @@
 	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<link rel="shortcut icon" href="assets/icon.png" type="image/x-icon">
-	<title>Войти</title>
+	<title>#TITLE#</title>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
