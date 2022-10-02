fd5ec810c35900e74db504a36eb938cd74d3de06
Vlad Kosarev <kva3292011@yandex.ru>
2022-10-02 20:54:00 +0300

    Добавил группировку по категориям в sql запрос

--- app/modules/catalog/lib/Category.php
+++ app/modules/catalog/lib/Category.php
@@ -14,9 +14,7 @@ class Category
 
 	public function getCategories() : array
 	{
-		$categories = [];
-		$sql = "SELECT `categories`.`name`, `categories`.`edit_date`, count(`catalog`.`product`) as 'product_count', `categories`.`preview_picture` FROM `categories` JOIN `catalog` ON `categories`.`id` = `catalog`.`category`;";
-		$categories = $this->db->sqlExecution($sql);
-		return $categories;
+		$sql = "SELECT `categories`.`id`, `categories`.`name`, `categories`.`edit_date`, count(`catalog`.`product`) as 'product_count', `categories`.`preview_picture` FROM `categories` JOIN `catalog` ON `categories`.`id` = `catalog`.`category` GROUP BY `categories`.`name`;";
+		return $this->db->sqlExecution($sql);
 	}
 }
